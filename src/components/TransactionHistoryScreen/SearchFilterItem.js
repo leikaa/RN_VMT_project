302b875f24b5be87224870e981332ddb6b02f661
Vyacheslav <myeve2007@yandex.ru>
2020-11-15 13:46:58 +0300

    feat: add date sort and correspondent name search at history screen

---
+++ src/components/TransactionHistoryScreen/SearchFilterItem.js
@@ -0,0 +1,74 @@
+import {
+  Dimensions,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import ArrowDownIcon from '../Common/Icons/ArrowDownIcon';
+import React from 'react';
+import Strings from '../../utils/strings';
+import {THEME} from '../../theme';
+
+const window = Dimensions.get('window');
+
+const SearchFilterItem = ({onPressSortItem, isOrderDesc, searchValue, setSearchValue}) => (
+  <View style={styles.item_container}>
+    <TouchableOpacity
+      onPress={onPressSortItem}
+
+      style={styles.item_sort_block}>
+      <Text style={styles.item_sort_block_text}>Date</Text>
+      <View style={!isOrderDesc && styles.item_sort_block_icon}>
+        <ArrowDownIcon color={THEME.MAIN_COLOR}/>
+      </View>
+
+    </TouchableOpacity>
+    <TextInput
+      style={styles.item_filter_input}
+      onChangeText={text => setSearchValue(text)}
+      placeholderTextColor={THEME.DISABLED_COLOR}
+      placeholder={Strings.placeholder_search_filter_item}
+      value={searchValue}
+    />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  item_container: {
+    flexWrap: 'nowrap',
+    flexDirection: 'row',
+    marginTop: 5,
+    height: 46,
+    backgroundColor: '#fff',
+    width: window.width - 40,
+    alignSelf: 'center',
+  },
+  item_sort_block: {
+    flexWrap: 'nowrap',
+    flexDirection: 'row',
+    width: '20%',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    backgroundColor: '#fff',
+  },
+  item_sort_block_text: {
+    marginRight: 5,
+  },
+  item_sort_block_icon: {
+    transform: [{rotate: '180deg'}],
+  },
+  item_filter_input: {
+    backgroundColor: '#fff',
+    color: '#3d3c3e',
+    height: 46,
+    width: '80%',
+    borderLeftWidth: 5,
+    borderLeftColor: '#f2f2f2',
+    paddingHorizontal: 15,
+  },
+
+});
+
+export default SearchFilterItem;
--- src/containers/TransactionsHistoryScreen.js
+++ src/containers/TransactionsHistoryScreen.js
@@ -8,23 +8,52 @@ import {
   Dimensions,
 } from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
+import moment from 'moment';
 
 import Header from '../components/Common/Header/Header';
 import HistoryItem from '../components/TransactionHistoryScreen/HistoryItem';
 import Preloader from '../components/Common/Preloader';
+import SearchFilterItem from '../components/TransactionHistoryScreen/SearchFilterItem';
 import {getTransactionsHistory} from '../store/actions/history';
 
 const window = Dimensions.get('window');
 
 const TransactionsHistoryScreen = () => {
   const userTransactionHistory = useSelector(state => state.History.transactionsHistory);
+  const balance = useSelector(state => state.Profile.balance);
   const token = useSelector(state => state.Authorization.token);
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(true);
+  const [isDescOrder, setIsDescOrder] = useState(true);
+  const [searchValue, setSearchValue] = useState('');
 
   useEffect(() => {
     dispatch(getTransactionsHistory(token, setIsLoading));
-  }, [userTransactionHistory]);
+  }, [balance]);
+
+  const sortData = (data, orderDesc) => {
+    if (orderDesc) {
+      data.sort((a, b) => new moment(b.date).format('YYYYMMDDHmmss') - new moment(a.date).format('YYYYMMDDHmmss'));
+    } else {
+      data.sort((a, b) => new moment(a.date).format('YYYYMMDDHmmss') - new moment(b.date).format('YYYYMMDDHmmss'));
+    }
+
+    return data;
+  };
+
+  const sortItemClickHandler = () => {
+    setIsDescOrder(!isDescOrder);
+  };
+
+  const filteredData = text => {
+    const initialData = sortData(userTransactionHistory, isDescOrder);
+
+    return initialData.filter(item => {
+      const itemData = `${item.username.toUpperCase()}`;
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) === 0;
+    });
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -37,23 +66,31 @@ const TransactionsHistoryScreen = () => {
       }
       {
         !isLoading &&
-        <View style={styles.content_container}>
+        <View style={styles.content_wrapper}>
           {
             userTransactionHistory.length > 0 ?
-              <FlatList
-                data={userTransactionHistory}
-                style={styles.items_list}
-                showsVerticalScrollIndicator={false}
-                renderItem={({item}) => (
-                  <HistoryItem item={item}/>
-                )}
-                keyExtractor={item => item.id.toString()}
+              <View tyle={styles.content_container}>
+                <SearchFilterItem
+                  onPressSortItem={sortItemClickHandler}
+                  isOrderDesc={isDescOrder}
+                  searchValue={searchValue}
+                  setSearchValue={setSearchValue}
+                />
+                <FlatList
+                  data={filteredData(searchValue)}
+                  style={styles.items_list}
+                  showsVerticalScrollIndicator={false}
+                  renderItem={({item}) => (
+                    <HistoryItem item={item}/>
+                  )}
+                  keyExtractor={item => item.id.toString()}
 
-                initialNumToRender={10}
-                updateCellsBatchingPeriod={50}
-                maxToRenderPerBatch={10}
-                windowSize={16}
-              />
+                  initialNumToRender={10}
+                  updateCellsBatchingPeriod={50}
+                  maxToRenderPerBatch={10}
+                  windowSize={16}
+                />
+              </View>
               :
               <View style={styles.no_history}>
                 <Text>You dont have any transactions</Text>
@@ -70,14 +107,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  content_container: {
+  content_wrapper: {
     flex: 1,
     backgroundColor: '#f2f2f2',
+  },
+  content_container: {
+    width: window.width - 40,
+    alignSelf: 'center',
   },
   items_list: {
     backgroundColor: '#f2f2f2',
     width: window.width - 40,
     alignSelf: 'center',
+    marginTop: 10, marginBottom: 55,
   },
   no_history: {
     flex: 1,
